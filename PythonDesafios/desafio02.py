f8b260fb9af6fc255fa5b7c60ae35729a70f5181
Emerson Martins <138334126+emersonmrd@users.noreply.github.com>
2024-04-11 13:15:31 -0300

    Aula 1 - 5

---
+++ PythonDesafios/desafio02.py
@@ -0,0 +1,4 @@
+diaNasc = input('Qual foi o dia em que você nasceu? ')
+mesNasc = input('Qual foi o mês em que você nasceu? ')
+anoNasc = input('Qual foi o ano em que vocês nasceu? ')
+print('Sua data de nascimento é:',diaNasc,'/',mesNasc,'/',anoNasc, ',Correto?') 
---
+++ PythonDesafios/desafio03.py
@@ -0,0 +1,3 @@
+num1 = input('Digite um número: ')
+num2 = input('Digite um segundo número: ')
+print = input('A soma é: ' + num1 + num2)
