7323a335ad8cc75647785d7e64537be935def3ad
Emerson Martins <138334126+emersonmrd@users.noreply.github.com>
2024-05-01 14:30:42 -0300

    melhora ex aula 08

--- PythonDesafios/desafio16.py
+++ PythonDesafios/desafio16.py
@@ -1,3 +1,3 @@
-import math
+from math import trunc
 num = float(input('Digite um número Real para saber sua parte inteira: '))
-print('O número {} tem sua parte inteira {}'.format(num,math.floor(num)))
+print('O número {} tem sua parte inteira {}'.format(num,trunc(num)))
--- PythonDesafios/desafio17.py
+++ PythonDesafios/desafio17.py
@@ -1,4 +1,10 @@
+import math
 co = float(input('Digite o valor do cateto oposto: '))
 ca = float(input('Digite o valor do cateto adjacente: '))
-hp = (co**2 + ca**2)**1/2
-print('O valor da hipotenusa desse triangulo é {}'.format(hp))
+hp = math.hypot(co,ca)
+print('O valor da hipotenusa desse triangulo é {:.2f}'.format(hp))
+
+'''co = float(input('Digite o valor do cateto oposto: '))
+ca = float(input('Digite o valor do cateto adjacente: '))
+hp = (co**2 + ca**2)**(1/2)
+print('O valor da hipotenusa desse triangulo é {:.2f}'.format(hp))'''
